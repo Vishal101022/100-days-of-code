89e800d3482e927aa083a0b5bc66483fcab85ca2
vishal101022 <raivishal939@gmail.com>
2022-12-14 13:39:45 +0530

    fibonacci series

---
+++ fibonacci.js
@@ -0,0 +1,10 @@
+// program to print fibonacci number 
+const fibonacci = (n) => {
+  const fibo = [0, 1];
+  for (let i = 2; i<n; i++) {
+    fibo[i] = fibo[i - 2] + fibo[i - 1];
+  }
+  return fibo;
+};
+
+console.log(fibonacci(10));
